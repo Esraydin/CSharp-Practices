a24ac1414934db97330279257d82cfcf84b75f10
Esra Aydın <aydinesra000@gmail.com>
2022-09-12 14:58:36 +0300

    Console-program

---
+++ console-program/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace console_program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                    Console.WriteLine("Hello, World!");
+                    Console.WriteLine("isminizi girin");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Soyisminizi girin:");
+                    string surname = Console.ReadLine();
+
+                   Console.WriteLine("Merhaba "  + name  + " " + surname );
+        }
+    }
+
+}
